187af412c6d0a8e863859ce03073a42b6fb3b7d0
deidg <64797316+deidg@users.noreply.github.com>
2022-10-10 14:13:34 +0300

    before fix the problem with 3 views

---
+++ Project 27 my places/CustomTableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CustomTableViewCell.swift
+//  Project 27 my places
+//
+//  Created by Alex on 10.10.2022.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+    
+    
+    
+}
