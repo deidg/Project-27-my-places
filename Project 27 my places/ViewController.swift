17880df2e59831ecd0eaa416df01db513a065e08
deidg <64797316+deidg@users.noreply.github.com>
2022-10-10 08:05:20 +0300

    Initial Commit

---
+++ Project 27 my places/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 27 my places
+//
+//  Created by Alex on 10.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
